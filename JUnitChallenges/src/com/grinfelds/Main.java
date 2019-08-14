247c194326063e297233ec6734dbf180dd655976
Mārtiņš Grinfelds <49753285+MartinsGrinfelds@users.noreply.github.com>
2019-08-14 20:09:50 +0300

    Add files via upload

---
+++ JUnitChallenges/src/com/grinfelds/Main.java
@@ -0,0 +1,11 @@
+package com.grinfelds;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Utilities util = new Utilities();
+        util.removePairs("AABCDDEFF");
+    }
+}
